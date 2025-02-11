9242ea8dc98062abcbc8ef14fa6b7cd2a17f3c52
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-02-11 12:16:58 +0500

    Update Loops.sh

--- Loops.sh
+++ Loops.sh
@@ -2,16 +2,17 @@
 z="Zeeshan"
 echo "My name is $z"
 
-echo "Loop Method : 1"
+echo "Loop Method : For-1"
 
 for ((i=0;i<=5;i++)); do
 echo "The muner is : $i"
 done
 
-echo "Loop Method : 2"
+echo "Loop Method : For-2"
 
 for i in {1..5}
 do
     echo "Iteration $i"
 done
+
 
