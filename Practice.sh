2725c20c5a7affcace1a1b3b42843e65a7135cd3
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-02-11 12:13:24 +0500

    Update Practice.sh

--- Practice.sh
+++ Practice.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+z="Zeeshan"
+echo "My name is $z"
+
+echo "Loop Method : 1"
+
+for ((i=0;i<=5;i++)); do
+echo "The muner is : $i"
+done
+
+echo "Loop Method : 2"
 
 for i in {1..5}
 do
     echo "Iteration $i"
 done
+
